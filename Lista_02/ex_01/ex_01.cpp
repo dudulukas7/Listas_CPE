9a908831c123f5cf98a4052e73b042df9d90c493
dudulukas7 <eduardo.lukasls.777@gmail.com>
2024-04-01 10:52:18 -0300

    colocando

---
+++ Lista_02/ex_01/ex_01.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    float x, y, z; 
+    x = ((7 + 3 * 6) /2 - 1);
+    cout << x << endl;
+    y = 2 % (2 + 2 * (2 / 2));
+    cout << y << endl;
+    z = (3 * 9 *(3+(9 * 3 / (3))));
+    cout << z << endl;
+} 
